baacc54cf1d5926a78cc93126dff2666580d053e
Mahdinkh7 <mahdinkh7@gmail.com>
2024-01-13 22:02:02 +0330

    create

---
+++ lib/animation.dart
@@ -0,0 +1,97 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+import 'model/partical.dart';
+import 'widgets/painter.dart';
+
+class ScoreScreans extends StatefulWidget {
+  const ScoreScreans({Key? key}) : super(key: key);
+
+  @override
+  _ScoreScreansState createState() => _ScoreScreansState();
+}
+
+class _ScoreScreansState extends State<ScoreScreans>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+  List<Particle> particles = [];
+
+  //this items for animations  createa random size
+  //and color Randoms
+
+  genereteListOfParticles() {
+    int numberOfParticles = 200;
+    for (int i = 0; i < numberOfParticles; i++) {
+      double randomSize = math.Random().nextDouble() * 20;
+      int randomR = math.Random().nextInt(256);
+      int randomG = math.Random().nextInt(256);
+      int randomB = math.Random().nextInt(256);
+
+      Color randomColor = Color.fromARGB(255, randomR, randomG, randomB);
+      double thetaRandom = math.Random().nextDouble() * (2 * math.pi);
+
+      double radiusRandom = math.Random().nextDouble() * 100 + 20;
+
+      Particle particle = Particle(
+          size: randomSize,
+          color: randomColor,
+          startingTheta: thetaRandom,
+          radius: radiusRandom);
+      particles.add(particle);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    genereteListOfParticles();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 8),
+    );
+    Tween<double> _rotationTween = Tween(begin: 0, end: 2 * math.pi);
+    animation = _rotationTween.animate(controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+
+    controller.repeat(max: 1);
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return WillPopScope(
+      onWillPop: () async {
+        ScaffoldMessenger.of(context);
+
+        return false;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: CustomPaint(
+          painter: MyPainter(particles: particles, theta: animation.value),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/painter.dart
@@ -0,0 +1,42 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+import '../model/partical.dart';
+
+class MyPainter extends CustomPainter {
+  final List<Particle> particles;
+  final double theta;
+  // ignore: unused_field
+  final Tween<double> _rotationTween = Tween(begin: 0, end: 2 * math.pi);
+
+  MyPainter({required this.particles, required this.theta});
+  @override
+  void paint(Canvas canvas, Size size) {
+    // paint brush
+    var paint = Paint()..strokeWidth = 5;
+
+    //generative art
+    // double radius = 200.0;
+
+    // ignore: avoid_function_literals_in_foreach_calls
+    particles.forEach((particle) {
+      double randomTheta = particle.startingTheta + theta;
+      double radius = particle.radius;
+
+      double dx = radius * theta * math.cos(randomTheta) + size.width / 2;
+      double dy = radius * theta * math.sin(randomTheta) + size.height / 2;
+
+      Offset position = Offset(dx, dy);
+
+      paint.color = particle.color;
+
+      canvas.drawCircle(position, particle.size, paint);
+    });
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
